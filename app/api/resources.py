0636d55292be86539bec64947b8364ebfde3e0a5
ivantana <ivan8tana@gmail.com>
2023-08-20 02:06:55 +0100

    change date format

--- app/api/resources.py
+++ app/api/resources.py
@@ -67,7 +67,7 @@ class User(Resource):
         args = user_request_parser.parse_args()
     
         args['user_type'] = USER_TYPES(args['user_type'])
-        args['birthday'] = datetime.strptime(args['birthday'], '%Y-%m-%d')
+        args['birthday'] = datetime.strptime(args['birthday'], '%d-%m-%y')
         args['password'] = generate_password_hash(args['password'])
         
  
